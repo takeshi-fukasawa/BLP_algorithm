9dbb05a98b6a480414f082689474b6ee029ed1f8
takeshi-fukasawa <fukasawa3431@gmail.com>
2024-03-20 13:49:30 +0900

    Additional change

--- BLP_algorithm/functions/delta_middle_Bellman_inner_func.m
+++ BLP_algorithm/functions/delta_middle_Bellman_inner_func.m
@@ -2,9 +2,9 @@ function [out,other_vars]=...
     delta_middle_Bellman_inner_func(...
     delta_initial,V_initial0,mu_ij,S_j_data,beta_C,L,...
     rho,weight,spectral_V_spec,tune_param_BLP,spec_default,...
-    weight_V,x_V)
+    weight_V,x_V,hot_start_V_spec)
 
-global feval_Bellman
+global feval_Bellman V_initial_hot_start
 
 spec=spec_default;
 if spectral_V_spec==1
@@ -13,8 +13,17 @@ else
     spec.update_spec=0;
 end
 
+if hot_start_V_spec==1
+    if feval_Bellman==0
+        V_initial_hot_start=V_initial0;
+    end
+else
+    V_initial_hot_start=V_initial0;
+end
+
+
         [output_spectral,other_vars_Bellman,iter_info_Bellman]=...
-        spectral_func(@Bellman_update_func,spec,{V_initial0},...
+        spectral_func(@Bellman_update_func,spec,{V_initial_hot_start},...
         delta_initial,mu_ij,beta_C,rho,...
         weight_V,x_V);
 
@@ -39,4 +48,6 @@ end
     other_vars.V_sol=V_sol;
     other_vars.s_jt_predict=s_jt_predict;
     
+    V_initial_hot_start=V_sol;
+
 end
--- BLP_algorithm/run_dynamic/run_BLP_middle_update_dynamic.m
+++ BLP_algorithm/run_dynamic/run_BLP_middle_update_dynamic.m
@@ -1,6 +1,7 @@
 
 global feval_Bellman
 spectral_V_spec=1;
+hot_start_V_spec=1;
 
 %% V_update_func 
 for method=2:2
@@ -14,7 +15,7 @@ for method=2:2
         spec,{delta_initial0},V_initial0,...
         mu_ijt_est,S_jt_data,beta_C,L,rho_est,...
         weight,spectral_V_spec,tune_param_BLP,spec,...
-        weight_V,x_V);
+        weight_V,x_V,hot_start_V_spec);
     tEnd=cputime;
 
     iter_info.t_cpu=tEnd-tStart; % inner and middle loop spectral algorithm => False toc...
