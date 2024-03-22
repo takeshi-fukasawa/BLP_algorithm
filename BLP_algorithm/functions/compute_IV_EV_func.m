b83f3a0c38031d2da9205e5b30748bfd14b46b18
takeshi-fukasawa <fukasawa3431@gmail.com>
2024-03-22 15:18:13 +0900

    minor

--- BLP_algorithm/functions/compute_IV_EV_func.m
+++ BLP_algorithm/functions/compute_IV_EV_func.m
@@ -3,11 +3,12 @@ function [numer_1,denom_1,IV,EV]=compute_IV_EV_func(...
 
     n_dim_V=size(V,5);
 
-    numer_1=exp(u_ijt_tilde./(1-rho));%J*ns*G*T*n_dim_V
-    denom_1=sum(numer_1,1);%1*ns*G*T*n_dim_V
-    IV=(1-rho).*log(denom_1);%1*ns*G*T*n_dim_V
 
-    
+    if n_dim_V==1
+        numer_1=exp(u_ijt_tilde./(1-rho));%J*ns*G*T*n_dim_V
+        denom_1=sum(numer_1,1);%1*ns*G*T*n_dim_V
+        IV=(1-rho).*log(denom_1);%1*ns*G*T*n_dim_V
+
     if n_dim_V>=2
         IV_obs_pt=IV(:,:,:,:,1);%1*ns*G*T
         IV=IVS_compute_IV_func(IV_obs_pt,n_dim_V-1);%1*ns*1*T*n_dim_V
--- BLP_algorithm/functions/compute_delta_from_V_func.m
+++ BLP_algorithm/functions/compute_delta_from_V_func.m
@@ -1,4 +1,4 @@
-%%%%%
+		%%%%%
 function [delta,Pr0,s_ijt_ccp_up_to_scale]=compute_delta_from_V_func(...
     mu_ijt,weight,S_jt_data,V)
 
@@ -15,7 +15,7 @@ function [delta,Pr0,s_ijt_ccp_up_to_scale]=compute_delta_from_V_func(...
     s_ijt_ccp_up_to_scale=exp(reshape(mu_ijt,J,ns,G,T,1)-...
         reshape(V(:,:,:,:,1),1,ns,1,T));%J*ns*G*T
 
-    Pr0=ones(1,ns,1,T+1);
+    Pr0=ones(1,ns,1,T);
 
    if Pr0_spec==0
         s_jt_up_to_scale=sum(reshape(weight,1,ns,1,1,1).*...
@@ -27,8 +27,9 @@ function [delta,Pr0,s_ijt_ccp_up_to_scale]=compute_delta_from_V_func(...
         delta=NaN(J,1,G,T);
 
        for t=1:T
+           Pr0_t=Pr0(:,:,:,t,:);
            s_jt_up_to_scale_t=...
-               sum(reshape(weight,1,ns,1,1).*Pr0(:,:,:,t,:).*...
+               sum(reshape(weight,1,ns,1,1).*Pr0_t.*...
                s_ijt_ccp_up_to_scale(:,:,:,t),2);%J*1*G*1
     
           exp_delta_t=S_jt_data(:,:,:,t)./s_jt_up_to_scale_t;%J*1*G*1
@@ -38,11 +39,10 @@ function [delta,Pr0,s_ijt_ccp_up_to_scale]=compute_delta_from_V_func(...
               exp_delta_t;%J*ns*G*1
           s_i0t_ccp_t=1-sum(s_ijt_ccp_t,[1,3]);%1*ns*1*1
 
-          Pr0(:,:,:,t+1)=Pr0(:,:,:,t).*s_i0t_ccp_t;%1*ns*1*1
-
+          if t<=T-1
+             Pr0(:,:,:,t+1)=Pr0_t.*s_i0t_ccp_t;%1*ns*1*1
+          end
        end % for loop
     end % if statement
-
-  Pr0=Pr0(:,:,:,1:end-1);%1*ns*1*T
 
 end
