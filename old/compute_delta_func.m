150ac603bed76c38bad9759178ed5b2143e04de3
takeshi-fukasawa <fukasawa3431@gmail.com>
2024-05-14 12:20:40 +0900

    minor

--- old/compute_delta_func.m
+++
@@ -1,8 +0,0 @@
-    %%%%%
-function delta=compute_delta_func(numer_1_without_delta,I_igt,weight,S_jt_data,rho,outside_util)
-
-    temp=(numer_1_without_delta.*exp(-rho./(1-rho).*I_igt))./(exp(outside_util)+sum(exp(I_igt),3));%J*ns*G*T
-
-    delta=(1-rho).*(log(S_jt_data)-log(sum(weight.*temp,2)));%J*1*G*T
-
-end
--- static_dynamic_BLP_algorithm/test_code.m
+++
@@ -1,6 +0,0 @@
-    %% IV_update static
-    tune_param=0;
-    run run_IV_update_static.m
-
-    tune_param=1;
-    run run_IV_update_static.m
