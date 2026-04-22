4ada308e405b9115059a747ee17f8c69b067a511
takeshi-fukasawa <fukasawa3431@gmail.com>
2026-04-22 12:15:18 +0900

    minor

--- run_experiments.py
+++ run_experiments.py
@@ -5,7 +5,7 @@ output_path="C:/Users/fukas/Dropbox/BLP/pyblp/"
 #exec(open("test_nevo.py").read())
 #exec(open("test_BLP.py").read())
 
-from turtle import pd
+import pandas as pd
 
 
 n_trial=3
@@ -31,6 +31,15 @@ Nevo_est_results_summary[8:11,[1,2]]=Nevo_est_results_summary[8:11,[1,2]]*(2*n_p
 with open(output_path+'Nevo_est_results_summary.csv', 'w',newline="") as f:
     writer = csv.writer(f)
     writer.writerows(Nevo_est_results_summary)
+
+results = pd.read_csv(output_path + "Nevo_est_results_summary.csv", header=None)
+results = results.rename(columns={4: "comp_time", 1: "obj_eval"})
+
+nonnumerical_time_per_obs = 0.098# cf. code profiling (profile_results_Nevo_numer_diff.csv)
+results['comp_time_numerical'] = results['comp_time'] - (nonnumerical_time_per_obs * results['obj_eval'])
+
+
+results.to_csv(output_path + "Nevo_est_results_summary_subtract_nonnumerical.csv", index=False)
 
 #########
 ## Summarise BLP results over trials
